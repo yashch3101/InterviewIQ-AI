229853f7ecf85e9abac3c8e7ff59494b8d205a0c
Yash Chaurasia <peterchaurasia@gmail.com>
2026-06-22 17:41:14 +0530

    AI-Powered Interview Preparation Platform

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -44,7 +44,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+})
 
     res.status(201).json({
         message: "User registered successfully",
@@ -88,7 +93,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+})
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -113,7 +123,11 @@ async function logoutUserController(req, res) {
         await tokenBlacklistModel.create({ token })
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
 
     res.status(200).json({
         message: "User logged out successfully"
