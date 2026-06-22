2fc5ebe868f7b46275e3c6f4c14ebddab70d5a46
Yash Chaurasia <peterchaurasia@gmail.com>
2026-06-22 16:53:54 +0530

    AI-Powered Interview Preparation Platform

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://interviewiq-ai-nxx8.onrender.com',
     withCredentials: true
 })
 
