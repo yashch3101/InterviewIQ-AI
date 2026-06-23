ae4df98fee251ca48a8f8ee8d8fed6c475f5e086
Yash Chaurasia <peterchaurasia@gmail.com>
2026-06-23 08:45:55 +0530

    fix vercel routing

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect  } from 'react'
 import { useNavigate, Link } from 'react-router'
 import { Mail, Lock, Sparkles } from 'lucide-react'
 import "../auth.form.scss"
@@ -12,6 +12,25 @@ const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const loadingMessages = [
+    "Connecting to AI Services...",
+    "Checking Authentication...",
+    "Preparing Dashboard...",
+    "Loading Interview Engine..."
+]
+
+    const [messageIndex, setMessageIndex] = useState(0)
+
+    useEffect(() => {
+        if (!loading) return
+
+        const interval = setInterval(() => {
+            setMessageIndex(prev => (prev + 1) % loadingMessages.length)
+        }, 2000)
+
+        return () => clearInterval(interval)
+    }, [loading])
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         await handleLogin({ email, password })
@@ -19,14 +38,34 @@ const Login = () => {
     }
 
     if (loading) {
-        return (
-            <main className="auth-page">
-                <div className="auth-loader">
-                    <h1>Loading...</h1>
+    return (
+        <main className="loading-screen">
+
+            <div className="loading-card">
+
+                <div className="loading-logo">
+                    AI Interview Coach
                 </div>
-            </main>
-        )
-    }
+
+                <div className="loader-ring"></div>
+
+                <h2>Preparing Your AI Experience</h2>
+
+                <p className="loading-message">
+                    {loadingMessages[messageIndex]}
+                </p>
+
+                <div className="loading-dots">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+            </div>
+
+        </main>
+    )
+}
 
     return (
         <main className="auth-page">
